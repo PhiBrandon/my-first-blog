3a798dcb11690077b72d6e37790478007260b3b5
Etherian <thedefinedone@gmail.com>
2017-06-19 15:05:31 -0700

    modified the post object variable to post all

--- blog/views.py
+++ blog/views.py
@@ -7,5 +7,5 @@ from .models import post
 
 # Create your views here.
 def post_list(request):
-    posts = post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = post.objects.all()
     return render(request, 'blog/post_list.html', {'posts' : posts})
